b44f51ad2f2a3191788e4e0eb41ef18039855c6a
KrystianZakrys <krystian.zakrys@gmail.com>
2025-02-05 23:23:12 +0100

    First Leetcode ever. TWO SUM

---
+++ TwoSum/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int,int> map = new Dictionary<int,int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var currentCheck = nums[i]; //bierzesz liczbe z tablicy
+            if (map.ContainsKey(target - currentCheck)) //sprawdzasz czy istnieje wartośc komplementarna do target
+            {
+                return [map[target - currentCheck], i]; // jeżeli tak to wypisz
+            }
+
+            map[currentCheck] = i; // jeżeli nie to dodaj do słownika sprawdzony element
+        }
+        return [];
+    }
+
+}
