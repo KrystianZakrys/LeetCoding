22e11ada9b62accd71227d1ea00bb3fcb2ef1330
KrystianZakrys <krystian.zakrys@gmail.com>
2025-02-10 13:41:14 +0100

    121. Best Time to Buy and sell stock

---
+++ BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class BestTimeToBuyAndSellStock
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int min = int.MaxValue;
+            int max = 0;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                min = prices[i] < min ? prices[i] : min;
+                max = (prices[i] - min) > max ? prices[i] - min : max;
+            }
+
+            return max;
+        }
+    }
+}
