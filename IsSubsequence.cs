8a98fd333d9be9dd081a80de0a22692e9d51bbba
KrystianZakrys <krystian.zakrys@gmail.com>
2025-02-10 12:50:15 +0100

    392. Is Subsequence

---
+++ IsSubsequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class IsSubsequence
+{
+    public class Solution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            int i = 0;
+            while (s.Length > 0 && i < t.Length)
+            {
+                if (s[0] == t[i])
+                {
+                    s = s.Substring(1);
+                }
+                i++;
+            }
+
+            return s.Length == 0;
+        }
+    }
+}
