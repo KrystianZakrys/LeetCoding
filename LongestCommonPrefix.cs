9c99ed6b37291121a6f08302e66dbcc88fb35d4a
KrystianZakrys <krystian.zakrys@gmail.com>
2025-02-10 16:58:38 +0100

    Longest common prefix

---
+++ LongestCommonPrefix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class LongestCommonPrefix
+{
+    public class Solution
+    {
+        string prefix = "";
+        public string LongestCommonPrefix(string[] strs)
+        {
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (i >= strs[j].Length || strs[j][i] != strs[0][i])
+                    {
+                        return prefix;
+                    }
+                }
+                prefix += strs[0][i];
+            }
+            return prefix;
+        }
+    }
+}
