2d2e7591075d4061f4d301b10881ce43e8b38b17
KrystianZakrys <krystian.zakrys@gmail.com>
2025-02-10 10:59:45 +0100

    2239. Find closest number to zero - simple iteration

---
+++ ClosestNum/Program.cs
@@ -0,0 +1,28 @@
+﻿
+var result = new Solution().FindClosestNumber([-1,2,3,4]);
+
+Console.WriteLine(result);
+public class Solution
+{
+    public int FindClosestNumber(int[] nums)
+    {
+        int currentClosest = int.MaxValue;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int absValue = Math.Abs(nums[i]);
+            int closestAbsValue = Math.Abs(currentClosest);
+
+            if (absValue < closestAbsValue)
+            {
+                currentClosest = nums[i];
+            }
+
+            if (absValue == closestAbsValue)
+                currentClosest = currentClosest > nums[i] ? currentClosest : nums[i];
+
+        }
+
+        return currentClosest;
+    }
+}
