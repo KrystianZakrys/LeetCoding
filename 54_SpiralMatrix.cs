e5840db0fbae80332d60f2f6ad76005769a363a4
KrystianZakrys <krystian.zakrys@gmail.com>
2025-02-22 09:42:08 +0100

    54 Spiral Matrix

---
+++ 54_SpiralMatrix.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Solution
+{
+    public IList<int> SpiralOrder(int[][] matrix)
+    {
+        List<int> result = new List<int>();
+        if (matrix.Length > 0)
+        {
+            int rowLength = matrix[0].Length - 1;
+            int matrixLength = matrix.Length - 1;
+            int top = 0;
+            int left = 0;
+            int maxElements = (matrixLength + 1) * (rowLength + 1);
+
+            while (result.Count < maxElements)
+            {
+                for (int i = left; i <= rowLength && result.Count < maxElements; i++)
+                {
+                    result.Add(matrix[top][i]);
+                }
+
+                top++;
+
+                for (int i = top; i <= matrixLength && result.Count < maxElements; i++)
+                {
+                    result.Add(matrix[i][rowLength]);
+                }
+                rowLength--;
+
+                for (int i = rowLength; i >= left && result.Count < maxElements; i--)
+                {
+                    result.Add(matrix[matrixLength][i]);
+                }
+                matrixLength--;
+
+                for (int i = matrixLength; i >= top && result.Count < maxElements; i--)
+                {
+                    result.Add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
